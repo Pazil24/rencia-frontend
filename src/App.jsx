d6300c86d7ef7b2422a0898e2ac6f51adfc8bd07
Pazil24 <azilafupriscilla@gmail.com>
2024-04-18 21:49:20 +0000

    updated navbar component and added a contacts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
+import PropertyListings from "./pages/PropertyListings";
 import SignUp from "./pages/SignUp";
 import Signin from "./pages/Signin";
+import ContactUs from "./pages/ContactUs";
+
 
 
 
 const App = () => {
   return <BrowserRouter>
+  <Navbar />
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about-us" element={<AboutUs /> } />
+    <Route path="/property-listings" element={<PropertyListings /> } />
     <Route path="/sign-up" element={<SignUp />} />
     <Route path="/sign-in" element={<Signin />} />
+    <Route path="/contact-us" element={<ContactUs />} />
   </Routes>
   </BrowserRouter>
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <div className="flex ">
+        <h1 className="font-bold">
+            <a href="/">Rencia</a>
+        </h1>
+        <nav>
+          <ul className="text-[#ee4c4c]">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about-us">About</a>
+            </li>
+            <li>
+              <a href="/property-listings">Properties</a>
+            </li>
+            <li>
+              <a href="/sign-in">Sign In</a>
+            </li>
+            <li>
+              <a href="/sign-up">Sign Up</a>
+            </li>
+            <li>
+              <a href="/contact-us">Reach Us</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
