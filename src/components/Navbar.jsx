4470e014008aafe4185a1eb04da947700c1a20ca
Pazil24 <azilafupriscilla@gmail.com>
2024-04-18 22:04:00 +0000

    added hover effect to navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,29 +2,29 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header>
-      <div className="flex ">
+    <header className="bg-slate-200">
+      <div className="flex  justify-between p-3">
         <h1 className="font-bold">
             <a href="/">Rencia</a>
         </h1>
         <nav>
-          <ul className="text-[#ee4c4c]">
-            <li>
+          <ul className="flex gap-5 mr-4 text-[#ee4c4c]">
+            <li className="hover:underline">
               <a href="/">Home</a>
             </li>
-            <li>
+            <li className="hover:underline">
               <a href="/about-us">About</a>
             </li>
-            <li>
+            <li className="hover:underline">
               <a href="/property-listings">Properties</a>
             </li>
-            <li>
+            <li className="hover:underline">
               <a href="/sign-in">Sign In</a>
             </li>
-            <li>
+            <li className="bg-[#8253e2]">
               <a href="/sign-up">Sign Up</a>
             </li>
-            <li>
+            <li className="hover:underline">
               <a href="/contact-us">Reach Us</a>
             </li>
           </ul>
